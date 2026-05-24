a72d1cd9c23cd12a080a7fd89af47254436b94e5
Kavya <kavyarani0510@hmail.com>
2026-05-24 17:33:29 +0530

    Simplify CORS

--- Backend/index.js
+++ Backend/index.js
@@ -10,14 +10,7 @@ const purchaseInvoiceRoutes = require("./routes/purchaseInvoiceRoutes");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 

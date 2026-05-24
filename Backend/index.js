8f7f807d81cc27fb8594e7a30c36f77fe8f10bf3
Kavya <kavyarani0510@hmail.com>
2026-05-24 14:58:45 +0530

    Add Vercel frontend to CORS

--- Backend/index.js
+++ Backend/index.js
@@ -13,9 +13,9 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
-      "https://retailiq-frontend.onrender.com",
-    ],
+  "http://localhost:3000",
+  "https://retail-q.vercel.app",
+],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })

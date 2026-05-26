06d18200a69db739183db08863b47c9b20f60162
Kavya <kavyarani0510@hmail.com>
2026-05-26 14:36:10 +0530

    updated cors

--- Backend/index.js
+++ Backend/index.js
@@ -11,17 +11,13 @@ const purchaseInvoiceRoutes = require("./routes/purchaseInvoiceRoutes");
 const app = express();
 
 /* ---------- CORS ---------- */
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://retail-q.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+app.use(
+  cors({
+    origin: "https://retail-q.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 /* ---------- Middleware ---------- */
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
